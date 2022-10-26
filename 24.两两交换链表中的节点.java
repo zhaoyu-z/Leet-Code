b792463b26c485c03a4b91760f82a6f90a5574ef
Zhayu517 <zhangzy517@gmail.com>
2022-10-26 15:10:17 +0100

    两两交换链表中的节点

---
+++ 24.两两交换链表中的节点.java
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=24 lang=java
+ *
+ * [24] 两两交换链表中的节点
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode nextNode = head.next;
+        head.next = swapPairs(nextNode.next);
+        nextNode.next = head;
+        return nextNode;
+    }
+}
+// @lc code=end
+
